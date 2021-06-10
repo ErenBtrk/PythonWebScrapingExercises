13618025375c514b1754943b366d84228a7378ed
= <=>
2021-06-10 15:00:56 +0300

    Exercise 15

--- Exercise15.py
+++ Exercise15.py
@@ -12,3 +12,9 @@ doc = html.fromstring(requests.get(url).text)
 pinlinks = [a for a in doc.cssselect('a') if 'pinterest.com' in str(a.attrib.get('href'))]
 print("The number of Pinterest accounts maintained by U.S. State Department embassies and missions:")
 print(len(pinlinks))
+
+iglinks = [a for a in doc.cssselect('a') if 'instagram.com' in str(a.attrib.get('href'))]
+print("The number of Instragram accounts maintained by U.S. State Department embassies and missions:")
+print(len(iglinks))
+for item in iglinks:
+    print(item.get("href"))
