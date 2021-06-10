639c8b7899b8d2989e376715f2f6368116bc52cc
= <=>
2021-06-10 17:29:32 +0300

    Exercise 23

--- Exercise23.py
+++ Exercise23.py
@@ -4,23 +4,39 @@
 
 '''
 
-import requests
 from bs4 import BeautifulSoup
+import requests
+import re
 
-url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+# Download IMDB's Top 250 data
+url = 'http://www.imdb.com/chart/top'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
 
-response = requests.get(url).text
-soup = BeautifulSoup(response,"html.parser")
+movies = soup.select('td.titleColumn')
+links = [a.attrs.get('href') for a in soup.select('td.titleColumn a')]
+crew = [a.attrs.get('title') for a in soup.select('td.titleColumn a')]
+ratings = [b.attrs.get('data-value') for b in soup.select('td.posterColumn span[name=ir]')]
+votes = [b.attrs.get('data-value') for b in soup.select('td.ratingColumn strong')]
 
-result = soup.find("tbody",{"class" : "lister-list"}).find_all("tr",limit=10)
-print(result)
+imdb = []
 
-count = 0
-for tr in result:
-    title = tr.find("td",{"class" : "titleColumn"}).find("a").text
-    year = tr.find("td",{"class" : "titleColumn"}).find("span").text.strip("()")
-    rating = tr.find("td",{"class" : "ratingColumn imdbRating"}).find("strong").text
-    count += 1
+# Store each item into dictionary (data), then put those into a list (imdb)
+for index in range(0, len(movies)):
+    # Seperate movie into: 'place', 'title', 'year'
+    movie_string = movies[index].get_text()
+    movie = (' '.join(movie_string.split()).replace('.', ''))
+    movie_title = movie[len(str(index))+1:-7]
+    year = re.search('\((.*?)\)', movie_string).group(1)
+    place = movie[:len(str(index))-(len(movie))]
+    data = {"movie_title": movie_title,
+            "year": year,
+            "place": place,
+            "star_cast": crew[index],
+            "rating": ratings[index],
+            "vote": votes[index],
+            "link": links[index]}
+    imdb.append(data)
 
-    print(f"{count} . Movie : {title.ljust(60)}  Year = {year}  Rating = {rating}")
-
+for item in imdb:
+    print(item['place'], '-', item['movie_title'], '('+item['year']+') -', 'Starring:', item['star_cast'])
